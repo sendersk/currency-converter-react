82f7580142602a3bcbebf91885d8935869803470
Przemyslaw Senderski <psenderski94@gmail.com>
2020-08-31 18:29:46 +0200

    currencies array created

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.4278,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.9384,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 3.9058,
+    },
+];
