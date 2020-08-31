7b6b802a3f89e74f9ecedda56a481e65f83d5ec8
Przemyslaw Senderski <psenderski94@gmail.com>
2020-08-31 22:25:36 +0200

    Updates

---
+++ precache-manifest.210b7abc9a7edcbd4ee4ae8ef98344bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b134e5cd269fd19c1841a53d46a779c",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "4c1cd1f38ef540f53ad1",
+    "url": "/currency-converter-react/static/css/main.7f0e577d.chunk.css"
+  },
+  {
+    "revision": "cb70966e9e6cb160b785",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1cd1f38ef540f53ad1",
+    "url": "/currency-converter-react/static/js/main.95d9b412.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  },
+  {
+    "revision": "26d1fafc01276591e47f05d4f4afe45a",
+    "url": "/currency-converter-react/static/media/background.26d1fafc.jpg"
+  }
+]);
