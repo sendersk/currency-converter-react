14df8ae9822f0323cd35473ff3b9d3f800e9b6e9
Przemyslaw Senderski <psenderski94@gmail.com>
2020-08-31 22:55:05 +0200

    Updates

--- precache-manifest.9a72d51564ba7687448029dd2cb74c8e.js
+++ precache-manifest.9a72d51564ba7687448029dd2cb74c8e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b134e5cd269fd19c1841a53d46a779c",
+    "revision": "edec6a7f9b6522af0298644ec8d26ab0",
     "url": "/currency-converter-react/index.html"
   },
   {
-    "revision": "4c1cd1f38ef540f53ad1",
-    "url": "/currency-converter-react/static/css/main.7f0e577d.chunk.css"
+    "revision": "0e243e7cb6a150d7d9f8",
+    "url": "/currency-converter-react/static/css/main.6cd07190.chunk.css"
   },
   {
     "revision": "cb70966e9e6cb160b785",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4c1cd1f38ef540f53ad1",
-    "url": "/currency-converter-react/static/js/main.95d9b412.chunk.js"
+    "revision": "0e243e7cb6a150d7d9f8",
+    "url": "/currency-converter-react/static/js/main.8af3a53c.chunk.js"
   },
   {
     "revision": "07b2973be230e9c120d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/currency-converter-react/precache-manifest.210b7abc9a7edcbd4ee4ae8ef98344bf.js"
+  "/currency-converter-react/precache-manifest.9a72d51564ba7687448029dd2cb74c8e.js"
 );
 
 self.addEventListener('message', (event) => {
