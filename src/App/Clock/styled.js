c66d5ef21ca4eca7a77acd107d90295911687d94
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-05 18:30:15 +0200

    theme color style added to Clock

--- src/App/Clock/styled.js
+++ src/App/Clock/styled.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     text-align: right;
     font-family: monospace;
-    color: #555;
+    color: ${({ theme }) => theme.color.emperor};
 `;
