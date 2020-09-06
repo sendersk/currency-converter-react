46552816701a018fb5d47e2073608a9575e99fa8
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-06 20:35:19 +0200

    App content modified

--- src/App/index.js
+++ src/App/index.js
@@ -1,35 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Form } from "./Form";
 import { Clock } from "./Clock";
-import { currencies } from "./currencies";
 import { Wrapper } from "./styled";
 
 function App() {
-  
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  }
 
   return (
     <Wrapper>
       <Clock />
-      <Form 
-        result={result}
-        calculateResult={calculateResult}
-      />
+      <Form />
     </Wrapper>
-  )
-
+  );
 }
 
 export default App;
