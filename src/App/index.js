79429df2fe858ccf0c19ee574fdf5a97256342c5
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-20 18:03:05 +0200

    App content rebuilt

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 import { Form } from "./Form";
 import { Clock } from "./Clock";
 import { Wrapper } from "./styled";
+import { SwitchTheme } from "./SwitchTheme";
+import { theme, darkTheme } from "../theme";
+import { ThemeProvider } from 'styled-components';
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "./themeSlice";
 
 function App() {
+  const isDarkTheme = useSelector(selectDarkTheme);
 
   return (
-    <Wrapper>
-      <Clock />
-      <Form />
-    </Wrapper>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : theme}>
+      <Wrapper>
+        <SwitchTheme />
+        <Clock />
+        <Form />
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
