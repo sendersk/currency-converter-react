e769316461497f44952ce08e1fe3939e6946a029
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-20 18:01:40 +0200

    darkTheme created

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,27 @@
 export const theme = {
     color: {
-        black: "#000",
-        emperor: "#555",
-        silver: "#ccc",
-        white: "#fff",
-        tumbleweed: "#daad86",
-        royalBlue: "#4c74ee",
-        crimson: "#db143c",
+        shadow: "#000",
+        infoColor: "#555",
+        buttonColor: "#fff",
+        field: "#daad86",
+        mainColor: "#4c74ee",
+        errorColor: "#db143c",
+        linkColor: "#3f50b5",
+        formBackground: "rgba(255,255,255,0.7)",
+        labelColor: "#000",
+    },
+};
+
+export const darkTheme = {
+    color: {
+        shadow: "#fff",
+        infoColor: "#ccc",
+        buttonColor: "#000",
+        field: "#0f4c75",
+        mainColor: "#bbe1fa",
+        errorColor: "#db143c",
+        linkColor: "#f2a365",
+        formBackground: "rgb(27, 38, 44, 0.7)",
+        labelColor: "#f2a07b",
     },
 };
